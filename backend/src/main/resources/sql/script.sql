79370fae3368ac7824cbff654766692576dc74ab
226wyj <308033240@qq.com>
2024-10-12 20:54:42 -0700

    update sql script to generate db

--- backend/src/main/resources/sql/script.sql
+++ backend/src/main/resources/sql/script.sql
@@ -1,3 +1,6 @@
+create database if not exists usercenter_db;
+use usercenter_db;
+
 create table if not exists user
 (
     id           bigint auto_increment
@@ -18,44 +21,3 @@ create table if not exists user
         unique (userAccount)
 )
     comment '用户';
-
-create table if not exists plan
-(
-    id              bigint auto_increment comment '计划 id'
-        primary key,
-    creatorId       bigint                             not null comment '创造者 id',
-    name            varchar(256)                       not null comment '计划名称',
-    courseDirection varchar(256)                       not null comment '课程方向',
-    subDirection    varchar(256)                       null comment '课程子方向',
-    courseTarget    text                               null comment '课程目标',
-    courseDetail    json                               null comment '课程安排',
-    comment         text                               null comment '备注',
-    isPublished     tinyint  default 0                 not null comment '是否发布，0 - 未发布，1 - 已发布',
-    isDelete        tinyint  default 0                 not null comment '是否删除',
-    createTime      datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime      datetime                           null comment '更新时间',
-    constraint plan_user_id_fk
-        foreign key (creatorId) references user (id)
-);
-
-create table if not exists teach
-(
-    id         bigint auto_increment
-        primary key,
-    studentId  bigint                             not null comment '学生 id',
-    teacherId  bigint                             not null comment '老师 id',
-    planId     bigint                             not null comment '教学计划 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime                           null comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除 0 - 否，1 - 是',
-    constraint student_user_id_fk
-        foreign key (studentId) references user (id)
-            on update cascade on delete cascade,
-    constraint teach_plan_id_fk
-        foreign key (planId) references plan (id)
-            on update cascade on delete cascade,
-    constraint teacher_user_id_fk
-        foreign key (teacherId) references user (id)
-            on update cascade on delete cascade
-);
-
